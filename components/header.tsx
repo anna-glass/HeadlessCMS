4f50be0c8a12c974e908c578d9dd1fdc14bd8095
anna-glass <anna@getslate.co>
2025-06-29 22:55:20 -0700

    profile page

--- components/header.tsx
+++ components/header.tsx
@@ -1,18 +1,18 @@
 import { stackServerApp } from '@/app/stack';
 import Link from 'next/link';
 import Image from 'next/image';
+import { UserButton } from '@stackframe/stack';
 
 export async function Header() {
   const user = await stackServerApp.getUser();
   const app = stackServerApp.urls;
-
 
   return (
     <header className="w-full flex justify-between items-center px-6 py-4 z-10">
       <div className="font-medium text-[15px] tracking-tight">
         <Image
           src="/logo.svg"
-          alt="logo logo"
+          alt="logo"
           width={102}
           height={28}
           priority
@@ -20,12 +20,7 @@ export async function Header() {
       </div>
       {user ? (
         <div className="flex items-center gap-4">
-          <Link
-            href={app.signOut}
-            className="bg-gray-50 px-1 underline text-[11px]  hover:no-underline"
-          >
-            Sign Out
-          </Link>   
+          <UserButton />
         </div>
       ) : (
         <div className="flex items-center gap-3">
@@ -37,7 +32,7 @@ export async function Header() {
           </Link>
           <Link
             href={app.signUp}
-            className="inline-flex h-8 items-center justify-center font-medium  text-center rounded-full outline-none   dark:text-black bg-primary-1 hover:bg-[#00e5bf] whitespace-nowrap px-6 text-[13px] transition-colors duration-200"
+            className="inline-flex h-8 items-center justify-center font-medium text-center rounded-full outline-none dark:text-black bg-primary-1 hover:bg-[#00e5bf] whitespace-nowrap px-6 text-[13px] transition-colors duration-200"
           >
             Sign Up
           </Link>
